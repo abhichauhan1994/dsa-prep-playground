c79a76a6d38051ff7a4c3253e564cd82aa02b81d
Abhishek <abhichauhan1994@gmail.com>
2026-04-14 14:32:19 +0530

    Add Main.java entry point

---
+++ src/main/java/org/play/Main.java
@@ -0,0 +1,10 @@
+package org.play;
+
+import org.play.slidingWindow.FixedSlidingWindow;
+
+public class Main {
+    public static void main(String[] args) {
+        FixedSlidingWindow.runMaxSumOfSubarrayOfSizeKTests();
+        FixedSlidingWindow.runMaxAverageSubArrayOfSizeKTests();
+    }
+}
