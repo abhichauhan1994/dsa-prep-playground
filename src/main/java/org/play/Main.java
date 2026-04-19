d23c43c8ab2d1f2315b54586d7ba0a137c36caeb
Abhishek <abhishek@example.com>
2026-04-19 19:22:30 +0000

    Add VariableSlidingWindow test calls to Main.java and application plugin

--- src/main/java/org/play/Main.java
+++ src/main/java/org/play/Main.java
@@ -1,10 +1,14 @@
 package org.play;
 
 import org.play.slidingWindow.FixedSlidingWindow;
+import org.play.slidingWindow.VariableSlidingWindow;
 
 public class Main {
     public static void main(String[] args) {
         FixedSlidingWindow.runMaxSumOfSubarrayOfSizeKTests();
         FixedSlidingWindow.runMaxAverageSubArrayOfSizeKTests();
+        VariableSlidingWindow.runMinimumSizeSubarraySumTests();
+        VariableSlidingWindow.runLongestSubstringTests();
+        VariableSlidingWindow.runMaxFruitsIntoBasketOfSameColorTests();
     }
 }
